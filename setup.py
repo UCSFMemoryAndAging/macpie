8ca2722573c57798d6cc42b687ed70e916a9d172
alee7 <albert.lee2@ucsf.edu>
2021-10-28 14:38:03 -0700

    bump all libraries: pandas 1.3.4; click 8.0.3; etc

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="macpie",
     version=version,
     install_requires=[
-        "click >= 8.0",
+        "click >= 8.0.3",
         "mysql-connector-python >= 8.0.26",
         "openpyxl >= 3.0",
         "pandas >= 1.2",
