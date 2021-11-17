f6de62b6272003e49b69ac3e7bf1bf4e1f4e0d3a
alee7 <albert.lee2@ucsf.edu>
2021-11-16 20:14:14 -0800

    docs: minor update to required libs

--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
         "tablib >= 3.0",
         "XlsxWriter >= 3.0",
     ],
-    extras_require={"dotenv": ["python-dotenv"], "mpsql": ["mysql-connector-python >= 8.0.26"]},
+    extras_require={
+        "dotenv": ["python-dotenv"],
+        "mpsql": ["mysql-connector-python >= 8.0.26", "SQLAlchemy >= 1.4"],
+    },
 )
