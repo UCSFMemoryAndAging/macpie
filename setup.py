3c4fb4f6f430e13b34bb91a8952b0adc92ea2a8d
alee7 <albert.lee2@ucsf.edu>
2022-04-18 00:02:32 -0700

    bump all libraries: pandas 1.4.2; click 8.1.2; etc

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name="macpie",
     install_requires=[
         "click >= 8.0.3",
-        "jinja2 >= 3.0",
         "openpyxl >= 3.0",
-        "pandas >= 1.2",
+        "pandas >= 1.3",
         "tablib >= 3.0",
         "XlsxWriter >= 3.0",
     ],
     extras_require={
         "dotenv": ["python-dotenv"],
         "mpsql": ["mysql-connector-python >= 8.0.26", "SQLAlchemy >= 1.4"],
+        "mpfile": ["tabulate >= 0.8.9"],
     },
 )
