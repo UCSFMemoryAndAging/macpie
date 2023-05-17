7340e4f28198cc324b642217bcd050e6fac5a505
alee7 <albert.lee2@ucsf.edu>
2023-05-17 15:20:26 -0700

    chore: update to version 0.6.0 in __init__.py

--- src/macpie/__init__.py
+++ src/macpie/__init__.py
@@ -1,23 +1,13 @@
 # flake8: noqa
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
-
-from macpie._config import get_option, set_option, reset_option
 
 # let init-time option registration happen
 import macpie.core.config_init
-
+from macpie import pandas, testing, util
+from macpie._config import get_option, reset_option, set_option
 from macpie.core.api import *
-
 from macpie.io.api import *
-
-from macpie.tools.api import *
-
 from macpie.pandas.accessors import MacDataFrameAccessor, MacSeriesAccessor
-
-from macpie import pandas
-
-from macpie import testing
-
-from macpie import util
+from macpie.tools.api import *
